7bc23607e158f18122a5944325d6710233359740
John <jcoronite@hotmail.com>
2016-03-23 08:01:53 -0700

    Refactor Layout Files

--- app/src/main/java/net/coronite/johncoroniteportfolio/MainActivity.java
+++ app/src/main/java/net/coronite/johncoroniteportfolio/MainActivity.java
@@ -2,12 +2,8 @@ package net.coronite.johncoroniteportfolio;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,37 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /** Called when the user touches the button */
@@ -59,27 +24,27 @@ public class MainActivity extends AppCompatActivity {
         CharSequence text = "";
         switch(view.getId())
         {
-            case (R.id.button):
+            case (R.id.content_main_btn_spotify_streamer):
                 text = "This button will launch my Streamer App";
                 break;
 
-            case (R.id.button2):
+            case (R.id.content_main_btn_scores_app):
                 text = "This button will launch my Scores App";
                 break;
 
-            case (R.id.button3):
+            case (R.id.content_main_btn_library_app):
                 text = "This button will launch my Library App";
                 break;
 
-            case (R.id.button4):
+            case (R.id.content_main_btn_build_it_bigger):
                 text = "This button will launch my Build It App";
                 break;
 
-            case (R.id.button5):
+            case (R.id.content_main_btn_xyz_reader):
                 text = "This button will launch my XYZ Reader App";
                 break;
 
-            case (R.id.button6):
+            case (R.id.content_main_btn_capstone_app):
                 text = "This button will launch my Capstone App";
                 break;
         }
